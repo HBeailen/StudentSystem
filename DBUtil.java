dff79665b51301ac8449b5d200ceaf549290989c
HBeailen <122974427+HBeailen@users.noreply.github.com>
2023-06-13 19:44:22 +0800

    untils01
    untils01

---
+++ DBUtil.java
@@ -0,0 +1,60 @@
+package com.roadjava.util;
+
+import java.sql.*;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/student?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+    private static final String DRIVER="com.mysql.jdbc.Driver";
+    private static final String USER_NAME="root";
+    private static final String PWD="jhb";
+
+    static{
+        try{
+            //com.mysql.jdbc.Driver 静态代码块
+            Class.forName(DRIVER); //要求JVM查找并加载指定的类，也就是JVM会执行该类的静态代码框。
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    //获取数据库连接
+    public static Connection getConn(){
+        try{
+            return DriverManager.getConnection(URL,USER_NAME,PWD);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void closeConn(Connection connection){
+        if(connection!=null){
+            try{
+                connection.close();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void closePs(PreparedStatement ps){
+        if(ps!=null){
+            try{
+                ps.close();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void closeRs(ResultSet rs){
+        if(rs!=null){
+            try{
+                rs.close();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
